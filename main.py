3c3d87df620d3acc1c1518e29ec9f7af0638770a
0zXD <wrenkain@LAPTOP-G7TQGQSE>
2024-09-10 11:56:05 +0530

    commit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import csv
 import os
 from processes import intch, enter_data, fail_analy, top_st, topper, cut_off, average
 
-while True: 
+while True:
     print("\n1. Import data from another CSV file")
     print("2. Enter data manually")
     print("3. Perform Analysis")
     print("4. Exit")
-    
+
     choice = intch("Enter your choice: ")
 
     if choice == 1:
@@ -17,8 +17,7 @@ while True:
             with open(filepath, 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 next(reader)
-                
-                data= []
+                data = []
                 for row in reader:
                     data.append(row)
 
@@ -26,15 +25,15 @@ while True:
 
                 if os.path.exists(file):
                     with open(file, 'a', newline='') as csvfile:
-                        header= ["Name","Roll_no","Marks", "Attendance"]
+                        header = ["Name", "Roll_no", "Marks", "Attendance"]
                         writeM = csv.writer(csvfile)
                         writeM.writerows(data)
                 else:
                     with open(file, 'w', newline='') as csvfile:
-                        header= ["Name","Roll_no","Marks", "Attendance"]
+                        header = ["Name", "Roll_no", "Marks", "Attendance"]
                         write = csv.writer(csvfile)
                         write.writerow(header)
-                        write.writerows(data)  
+                        write.writerows(data)
             print("Data added succesfully.")
 
         except Exception as e:
@@ -43,8 +42,8 @@ while True:
         print("**************************************")
         print("\n1) List by Cut-Off. \n2) Topper \n3) Students Failed. \n4) Top students list. \n5) Average marks. \n6) Exit.... ")
         choose = intch("Choose option: ")
-        
-        if choose ==1:
+
+        if choose == 1:
             cut_off()
 
         elif choose == 2:
@@ -62,17 +61,15 @@ while True:
         elif choose == 6:
             break
 
-
-
-    if choice == 2: 
+    if choice == 2:
         print("Enter the data manually:")
         data = enter_data()
 
         print("**************************************")
         print("\n1) List by Cut-Off. \n2) Topper \n3) Students Failed. \n4) Top students list. \n5) Average marks. \n6) Exit....")
         choose1 = intch("Choose option: ")
-        
-        if choose1 ==1:
+
+        if choose1 == 1:
             cut_off()
 
         elif choose1 == 2:
@@ -94,8 +91,8 @@ while True:
         print("**************************************")
         print("\n1) List by Cut-Off. \n2) Topper \n3) Students Failed. \n4) Top students list. \n5) Average marks. \n6) Exit....")
         choose2 = intch("Choose option: ")
-        
-        if choose2 ==1:
+
+        if choose2 == 1:
             cut_off()
 
         elif choose2 == 2:
@@ -112,11 +109,8 @@ while True:
 
         elif choose2 == 6:
             break
-   
 
-    
     if choice == 4:
         print("Exiting...")
         print("**************************************")
         break
-
